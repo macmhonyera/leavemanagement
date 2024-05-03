4066e9df62abe2db75ffb664fc89b0d163413144
macmhonyera <macmhonyera@gmail.com>
2024-05-03 11:24:55 +0200

    configure swagger and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
-  imports: [],
+  imports: [
+   TypeOrmModule.forRootAsync({
+    imports:[ConfigModule.forRoot({isGlobal:true})],
+    inject:[ConfigService],
+    useFactory:async(configService:ConfigService)=>({
+      type:"postgres",
+        host:configService.get('DB_HOST'),
+        port: configService.get("DB_PORT"),
+        username: configService.get("DB_USER"),
+        password: configService.get("DB_PASSWORD"),
+        database: configService.get("DB_NAME"),
+        entities: [__dirname + '/../**/*.entity.{.js,.ts}'],
+        autoLoadEntities: true,
+        synchronize: true,
+        logging: true,
+    })
+   })
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+  const config=new DocumentBuilder()
+  .setTitle('Leave management')
+  .setDescription('Leave management api')
+  .setVersion('1.0')
+  .addTag('Leave management')
+  .addBearerAuth(
+    {
+      type:'http',
+      scheme:'bearer',
+      bearerFormat:'JWT',
+      name:'JWT',
+      description:'Enter the token here:',
+      in:'header'
+    }
+  )
+  .build()
+
+  const document=SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,document)
+
+  const configService:ConfigService=app.get(ConfigService);
+
+  await app.listen(configService.get("PORT","0.0.0.0"))
+  console.log("Listening on port:" +configService.get("PORT"))
 }
 bootstrap();
